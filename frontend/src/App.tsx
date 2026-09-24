93f45c86555cef5319c99aef65f99c22b1e363dc
Jacob <JacobZapp01@outlook.com>
2026-09-24 10:31:04 -0500

    Set up the frontend, React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Pokémon Team Planner</h1>
+      <p>Build and save teams for your favorite Pokémon games.</p>
+    </main>
+  )
+}
+
+export default App
